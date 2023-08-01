1f79ad74ef17ffb18c137ffcdc4930ab0ff6b407
Aleksei Sousa <112890976+saiko1243@users.noreply.github.com>
2023-08-01 17:05:59 -0300

    sidebar

--- app/javascript.js
+++ app/javascript.js
@@ -1,17 +1,88 @@
-    /*$(".demo").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [{
-            breakpoint: 800,
-                settings: {
-                    slidesToShow: 2,
-                    dots: true
-                },
-                breakpoint: 300,
-                settings: "unslick"
-        }]
-    });*/
+const btn = [...document.querySelectorAll('.link-nav')]
+let botao = [...document.querySelectorAll('.link-nav')]
+const main = document.querySelector('main') 
+botao.map((e)=>{
+    //e.classList.remove('nav-btn-ativa')
+})
+btn.map((ele) => {
+    ele.addEventListener('click', ()=>{
+        classe = 'link-nav-ativa'
+        removerClasse(btn,classe)
+        adicionarClasse(ele, btn,classe)
 
- 
+        /*let elemento =''
+        switch (ele.id){
+            case "btn_alteracoes1":
+                elemento = document.querySelector("#btn_alteracoes1")
+            break;
+            case "btn_alteracoes2":
+                elemento = document.querySelector("#btn_alteracoes2")
+            break;
+            case "btn_alteracoes3":
+                elemento = document.querySelector("#btn_alteracoes3")
+            break;
+        alterarClasse(array, elemento, classe)
+
+        }
+
+        classe = 'form_dados_desativada'
+        array = [...document.querySelectorAll('.form_alteracoes')]*/
+    })
+    
+});
+
+const removerClasse = (array, classe)=>{
+    array.map((e)=>{
+        e.classList.remove(classe)
+        console.log("removido")
+    })
+    //elemento.classList.toggle(classe)
+}
+
+const adicionarClasse = (ele, array, classe)=>{
+
+        ele.classList.add(classe)
+        console.log("add")
+
+    //elemento.classList.toggle(classe)
+}
+
+/*btn.map((ele) => {
+    ele.addEventListener('click', ()=>{
+        let ele2 = document.querySelector('.form_alteracoes')
+        classe = "form_dados"
+        alterarClasse(ele2, classe)
+    })
+});*/
+
+let contadorSide = false
+let navbar = document.querySelector('.nav-btns')
+const sidebar = ()=>{
+    contadorSide = !contadorSide;
+   if(contadorSide){
+        navbar.style.right = '0px'
+        main.style.filter = 'brightness(0.8)'
+    } else {
+        navbar.style.right = '-250px'
+        main.style.filter = ''
+    }
+
+    /*if (navbar.style.right == '-250px'){
+        navbar.style.right = '0px'
+        console.log(navbar.style.right)
+    }else if(navbar.style.right == '0px') {
+        navbar.style.right = '-250px'
+    }*/
+
+}
+function fecharSideBar () {
+    if(contadorSide){
+        sidebar()
+    }
+}
+
+window.addEventListener('resize', (e)=>{
+    if(window.innerWidth > 800 && contadorSide){
+        sidebar()
+    }
+})
